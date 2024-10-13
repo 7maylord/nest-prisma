7b45ada3e6dafe74aa1204c21037da3f327dc3e2
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-10-13 13:58:25 +0100

    chore: Update Post Module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
